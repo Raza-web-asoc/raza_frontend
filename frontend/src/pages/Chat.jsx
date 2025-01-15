b57b45198154299add5a534cf29c6342073120aa
JuanCarlosPrieto <juan.carlos@caseware.com>
2025-01-14 23:58:37 -0500

    Chat, segundo commit

--- frontend/src/pages/Chat.jsx
+++ frontend/src/pages/Chat.jsx
@@ -1,22 +1,33 @@
 import React, { useState, useEffect } from "react";
 import { getChatsByPet, getUniqueChatByPets } from "../services/chatsServices/getChatsService";
+import { getPetById } from "../services/petsServices/getPetsService";
 import { sendMessage } from "../services/chatsServices/sendMessageService";
 import ChoosePetModal from "../components/Match/ChoosePet";
 
 const Chat = () => {
   const [isModalOpen, setIsModalOpen] = useState(true);
   const [selectedPet, setSelectedPet] = useState(null);
-  const [petIds, setPetIds] = useState([]);
+  const [pets, setPets] = useState([]);
   const [selectedPetId, setSelectedPetId] = useState(null);
   const [chat, setChat] = useState([]);
   const [message, setMessage] = useState("");
 
+  const getPetName = async (petId) => {
+    const pet = await getPetById(petId);
+    console.log(pet);
+    return pet.data.nombre_mascota;
+  }
+
   useEffect(() => {
     if (selectedPet) {
-      const fetchChats = async () => {
-        const chats = await getChatsByPet(selectedPet.id_mascota);
-        setPetIds(chats);
-      };
+        const fetchChats = async () => {
+            const chats = await getChatsByPet(selectedPet.id_mascota);
+            const petsData = await Promise.all(chats.map(async (petId) => {
+              const petName = await getPetName(petId);
+              return { id_mascota: petId, nombre_mascota: petName };
+            }));
+            setPets(petsData);
+        };
       fetchChats();
     }
   }, [selectedPet]);
@@ -24,15 +35,22 @@ const Chat = () => {
   const selectChat = async (petId) => {
     const chat = await getUniqueChatByPets(selectedPet.id_mascota, petId);
     setChat(chat);
-    setSelectedPetId(petId);
-  };
+    const pet = pets.find(pet => pet.id_mascota === petId);
+    setSelectedPetId(pet.id_mascota);
+  };  
 
-  const handleSendMessage = () => {
+  const handleSendMessage = async () => {
     if (message.trim()) {
-      sendMessage(selectedPet.id_mascota, selectedPetId, message);
-      setMessage("");
+      try {
+        await sendMessage(selectedPet.id_mascota, selectedPetId, selectedPet.id_mascota, message);
+        setMessage(""); // Clear the message after sending
+      } catch (error) {
+        console.error("Error sending message:", error);
+        // Optionally show an error message
+      }
     }
   };
+  
 
   return (
     <div className="flex h-screen">
@@ -48,17 +66,21 @@ const Chat = () => {
           {selectedPet && <p className="mt-4">Mascota: {selectedPet.nombre_mascota}</p>}
         </div>
         <div>
-          {petIds.map((petId) => (
-            <div
-              key={petId}
-              onClick={() => selectChat(petId)}
-              className={`p-4 cursor-pointer border-b border-gray-700 ${
-                selectedPetId === petId ? "bg-gray-700" : "hover:bg-gray-700"
-              }`}
-            >
-              {petId}
-            </div>
-          ))}
+        {pets.length > 0 ? (
+            pets.map((pet) => (
+                <div
+                key={pet.id_mascota}
+                onClick={() => selectChat(pet.id_mascota)}
+                className={`p-4 cursor-pointer border-b border-gray-700 ${
+                    selectedPetId === pet.id_mascota ? "bg-gray-700" : "hover:bg-gray-700"
+                }`}
+                >
+                {pet.nombre_mascota}
+                </div>
+            ))
+            ) : (
+            <div className="text-gray-500 text-center mt-20">Cargando mascotas...</div>
+        )}
         </div>
       </div>
 
@@ -67,9 +89,9 @@ const Chat = () => {
         <div className="flex-1 p-4 overflow-y-auto">
           {chat.length > 0 ? (
             chat.map((msg) => (
-              <div key={msg.message_id} className="p-2 mb-2 bg-gray-200 rounded-lg max-w-xs">
-                {msg.message}
-              </div>
+                <div key={msg.message_id} className={`p-2 mb-2 ${msg.pet_id !== selectedPet.id_mascota ? "bg-gray-200" : "bg-blue-200"} rounded-lg max-w-xs ${msg.pet_id === selectedPet.id_mascota ? "self-end ml-auto" : ""}`}>
+                  {msg.message}
+                </div>
             ))
           ) : (
             <div className="text-gray-500 text-center mt-20">Selecciona un chat para ver mensajes</div>
--- frontend/src/services/chatsServices/sendMessageService.js
+++ frontend/src/services/chatsServices/sendMessageService.js
@@ -2,12 +2,13 @@ import axios from "axios";
 
 export const sendMessage = async (pet_id1, pet_id2, pet_id_sender, message) => {
   const token = localStorage.getItem("access_token");
+  console.log(pet_id1, pet_id2, pet_id_sender, message);
   if (!token) {
     throw new Error("Token no encontrado");
   }
   try {
     const response = await axios.post(
-      "http://localhost/api/chats/save-message/",
+      "http://localhost/api/chats/save-message",
       {pets: [pet_id1, pet_id2], pet_id: pet_id_sender, message: message},
       {
         headers: {
